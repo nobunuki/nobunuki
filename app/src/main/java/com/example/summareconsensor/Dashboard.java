8a081702f627d21cf973161f68ead7e8abe827fd
typonemma <glentong0@gmail.com>
2022-08-21 22:08:29 +0700

    masih error di firebase

--- app/src/main/java/com/example/summareconsensor/Dashboard.java
+++ app/src/main/java/com/example/summareconsensor/Dashboard.java
@@ -2,15 +2,40 @@ package com.example.summareconsensor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class Dashboard extends AppCompatActivity {
-
+ImageButton soilmoisturebtn;
+ImageButton airflowbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        soilmoisturebtn = (ImageButton) findViewById(R.id.btnsoil);
+        soilmoisturebtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), sensorsoilmostuire.class);
+                startActivity(intent);
+            }
+        });
+
+        airflowbtn = (ImageButton) findViewById(R.id.btnairquality);
+        airflowbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), sensorairflow.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
