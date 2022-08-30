392a02b5c8690fa62c282e2771ef51101c7f15a9
typonemma <glentong0@gmail.com>
2022-08-30 13:56:16 +0700

    circle gauge

--- app/src/main/java/com/example/summareconsensor/sensorsoilmostuire.java
+++ app/src/main/java/com/example/summareconsensor/sensorsoilmostuire.java
@@ -4,6 +4,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.google.firebase.firestore.DocumentReference;
@@ -14,7 +15,9 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class sensorsoilmostuire extends AppCompatActivity {
     private TextView dataair;
+    private ProgressBar progress_bar;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,8 @@ public class sensorsoilmostuire extends AppCompatActivity {
 
         DocumentReference data1out = db.collection("SummareconSensor2").document("Soil");
         dataair = findViewById(R.id.data1number);
+        progress_bar = findViewById(R.id.progress_bar);
+
 
 
         data1out.addSnapshotListener(new EventListener<DocumentSnapshot>() {
@@ -31,14 +36,22 @@ public class sensorsoilmostuire extends AppCompatActivity {
                                 @Nullable FirebaseFirestoreException e) {
 
                 Double dataA = snapshot.getDouble("a");
+                int i = Integer.valueOf(dataA.intValue());
+                progress_bar.setProgress(i);
+
+
                 String txtDataA = String.valueOf(dataA);
-                dataair.setText(txtDataA);
+               dataair.setText(txtDataA);
 
             }
         });
 
 
 
+
+    }
+
+    private void updateProgressBar() {
 
     }
 }
