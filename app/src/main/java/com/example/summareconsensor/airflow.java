100683cafd6b10a82ec2184ace95aab081cf103a
heldi <heldidiwig@gmail.com>
2022-09-26 11:49:44 +0700

    asd

--- app/src/main/java/com/example/summareconsensor/airflow.java
+++ app/src/main/java/com/example/summareconsensor/airflow.java
@@ -2,6 +2,7 @@ package com.example.summareconsensor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -13,17 +14,19 @@ import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
 public class airflow extends AppCompatActivity {
-    private TextView txtprogresss, txtinfo ;
+    private TextView progress_text_air, info_text_air, temperature_air;
     private Firebase myRef ;
     private ProgressBar progress_bar;
 
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_airflow);
-        txtprogresss = (TextView)findViewById(R.id.text_view_progress);
-        txtinfo = (TextView)findViewById(R.id.text_view_info); 
+        progress_text_air = findViewById(R.id.text_view_progressair);
+        info_text_air = findViewById(R.id.text_view_infoair);
+        temperature_air = findViewById(R.id.txttemperatureair);
 
         progress_bar = findViewById(R.id.progress_bar);
         Firebase.setAndroidContext(this);
@@ -32,19 +35,25 @@ public class airflow extends AppCompatActivity {
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                String sensor = dataSnapshot.child("airhumidity").getValue(String.class);
-                int air = Integer.parseInt(sensor.toString());
-                txtprogresss.setText(sensor);
+                String sensorair = dataSnapshot.child("airhumidity").getValue(String.class);
+                String sensortemperatureair = dataSnapshot.child("airtemperature").getValue(String.class);
+                int air = Integer.parseInt(sensorair.toString());
+                temperature_air.setText(sensortemperatureair + "°C");
+                progress_text_air.setText(sensorair);
                 progress_bar.setProgress(air);
-
+                if(air<=50){
+                    info_text_air.setText("Dry");
+                }else{
+                    info_text_air.setText("Humid");
+                }
+                progress_bar.setMax(100);
+                progress_bar.setProgress(air);
             }
-
             @Override
             public void onCancelled(FirebaseError firebaseError) {
                 Toast.makeText(getApplicationContext(), "Error, Please Contact our Administrator!",
                         Toast.LENGTH_LONG).show();
             }
         });
-
     }
 }
--- app/src/main/java/com/example/summareconsensor/sensorsoilmostuire.java
+++ app/src/main/java/com/example/summareconsensor/sensorsoilmostuire.java
@@ -4,6 +4,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
@@ -21,39 +22,42 @@ import com.firebase.client.ValueEventListener;
 import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 public class sensorsoilmostuire extends AppCompatActivity {
-    private TextView dataair, progress_text, info_text;
+    private TextView progress_text_soil, info_text_soil, temperature_soil;
     private ProgressBar progress_bar;
     private Firebase myRef;
 
 
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensorsoilmostuire);
 
-        dataair = findViewById(R.id.data1);
         progress_bar = findViewById(R.id.progress_bar);
-        progress_text = findViewById(R.id.text_view_progress);
-        info_text = findViewById(R.id.text_view_info);
+        progress_text_soil = findViewById(R.id.text_view_progresssoil);
+        info_text_soil = findViewById(R.id.text_view_infosoil);
+        temperature_soil = findViewById(R.id.txttemperaturesoil);
         Firebase.setAndroidContext(this);
         myRef = new Firebase("https://summareconsensor-default-rtdb.firebaseio.com/sensor");
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                String sensor = dataSnapshot.child("soilmoisture").getValue(String.class);
-                int soil = Integer.parseInt(sensor.toString());
-                progress_text.setText(sensor);
+                String sensorsoil = dataSnapshot.child("soilmoisture").getValue(String.class);
+                String sensortemperaturesoil = dataSnapshot.child("soiltemperature").getValue(String.class);
+                int soil = Integer.parseInt(sensorsoil.toString());
+                progress_text_soil.setText(sensorsoil);
+                temperature_soil.setText(sensortemperaturesoil);
 
-                if(soil<40){
-                    info_text.setText("Kurang Air");
-                }else if(soil<80){
-                    info_text.setText("Cukup Baik!");
-                }else if (soil >100){
-                    info_text.setText("Bagus!");
+                if(soil<=40){
+                    info_text_soil.setText("Kering");
+                }else if(soil > 40 && soil <= 70){
+                    info_text_soil.setText("Normal");
                 }else{
-                    Toast.makeText(getApplicationContext(), "Error, Please Contact our Administrator!",
-                            Toast.LENGTH_LONG).show();
+                    info_text_soil.setText("Basah");
+                //}else{
+                    //Toast.makeText(getApplicationContext(), "Error, Please Contact our Administrator!",
+                            //Toast.LENGTH_LONG).show();
                 }
 
                 progress_bar.setMax(100);
@@ -66,10 +70,5 @@ public class sensorsoilmostuire extends AppCompatActivity {
                         Toast.LENGTH_LONG).show();
             }
         });
-
-
-
-
-
     }
 }
